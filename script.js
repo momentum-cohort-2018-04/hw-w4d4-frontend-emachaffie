44ef6d215e66b8b3429595a3f16d873cf8d0e360
emachaffie <emachaffie@gmail.com>
2018-05-17 13:47:13 -0400

    HTML mockup made and started pseudocode for js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 import $ from 'jquery'
 import request from 'superagent'
 
+// Function to listen for submit on search bar
+
+// Function to get from API on success of that function
+
+// Function to turn results from API into HTML
+
+// Function to put HTML divs into HTML
+
+// Function to play the song when album-div is clicked on
+
+// Function to clear results of last search when new search is done?
