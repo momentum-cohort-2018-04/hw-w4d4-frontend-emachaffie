c08bec578950ffa21fa2acae08a4e233e24aa24b
emachaffie <emachaffie@gmail.com>
2018-05-17 19:11:48 -0400

    More work on functions and some styling

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 import $ from 'jquery'
 import request from 'superagent'
 window.$ = $
-// Function to turn input field into searchterm var
-// let searchterms = document.getElementById('searchinput').value
+// Did the above because console was doing something else with $ that made it look like it was taking jquery but it wasn't
 
-// Function to listen for submit on search bar
+// Function to listen for submit on search bar and to get value of input in search bar, send to API, return results and store in variable
 $(document).on('submit', '#searchform', function (event) {
   event.preventDefault()
   let $searchterms = $('#searchinput').val()
@@ -12,22 +11,45 @@ $(document).on('submit', '#searchform', function (event) {
   console.log('Search button clicked')
   request
     .get(`https://itunes.apple.com/search?term=${$searchterms}&media=music`)
-    // .get('https://itunes.apple.com/search?term=rancid&media=music')
     .then(response => {
-      console.log(response.body)
-      let searchresults = response.body.results
+      let parsedResponse = JSON.parse(response.text)
+      // Did above because the iTunes API doesn't return results in JSON format and superagent expects JSON
+      console.log(parsedResponse)
+      let searchresults = parsedResponse.results
+      getTrackInfo()
+      console.log(albumImage)
+      console.log(artist)
+      console.log(track)
+      // console.log (searchresults)
+      // resultsToHTML()
+      // console.log(resultsToHTML)
     }
     )
 })
 
-// Function to turn results from API into HTML
+// function
+// This make function for one item in array, then call this one in a separate function
+
+// FUNCTION BELOW gets called in above so that searchresults has access to that value? 
+
+function getTrackInfo () {
+  searchresults.forEach(element => {
+    let albumImage = track.artworkUrl60
+    let artist = track.artistName
+    let songTitle = track.trackName
+  })
+}
+// ${searchresults[i]}
+
+// Function to insert getTrackInfo HTML into HTML
+// Do I take searchresults as the argument???// for each or map?
+
 function resultsToHTML (searchresults) {
-  // Do I take searchresults as the argument???
   return `
   <div class = "album-div">
-        <img class = "album-img" src="media/clashalbum.jpg">
-        <p class = "artistname">The Clash</p>
-        <p class = "songtitle">Death or Glory</p>
+        <img class = "album-img" src="${albumImage}">
+        <p class = "artistname">${artist}</p>
+        <p class = "songtitle">${songTitle}</p>
       </div>
   `
 }
